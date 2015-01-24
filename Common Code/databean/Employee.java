80790c8f8ceb1fdf8c1e7e4401395e70b21d90f1
Eddie <mitchneeil@gmail.com>
2015-01-24 17:53:41 -0500

    employee bean

--- Common Code/databean/Employee.java
+++ Common Code/databean/Employee.java
@@ -6,124 +6,85 @@ import java.util.Random;
 
 import org.genericdao.PrimaryKey;
 
+
+
+
 @PrimaryKey("username")
 public class Employee implements Comparable<Employee> {
-	private int employeeId;
-	private String username = null;
-	private String hashedPassword = "*";
-	private int salt = 0;
-	private String employeeFirstName = null;
-	private String employeeLastName = null;
+	private int employee_id;
+	private String  username = null;
+	private int     salt           = 0;
+	private String password;
+	private String  firstname      = null;
+	private String  lastname       = null;
+	
+
+	
 
 	public boolean checkPassword(String password) {
-		return hashedPassword.equals(hash(password));
+		return this.password.equals(password);
 	}
-
+	
 	public int compareTo(Employee other) {
 		// Order first by lastName and then by firstName
-		int c = employeeLastName.compareTo(other.employeeLastName);
-		if (c != 0)
-			return c;
-		c = employeeFirstName.compareTo(other.employeeFirstName);
-		if (c != 0)
-			return c;
+		int c = lastname.compareTo(other.lastname);
+		if (c != 0) return c;
+		c = firstname .compareTo(other.firstname );
+		if (c != 0) return c;
 		return username.compareTo(other.username);
 	}
 
 	public boolean equals(Object obj) {
 		if (obj instanceof Employee) {
 			Employee other = (Employee) obj;
-			return (employeeId == employeeId);
+			return (employee_id ==employee_id);
 		}
 		return false;
 	}
 
-	public int getEmployeeId() {
-		return this.employeeId;
-	}
-
-	public String getHashedPassword() {
-		return hashedPassword;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public int getSalt() {
-		return salt;
-	}
-
-	public String getEmployeeFirstName() {
-		return employeeFirstName;
-	}
-
-	public String getEmployeeLastName() {
-		return employeeLastName;
-	}
-
-	public int hashCode() {
-		return username.hashCode();
-	}
-
-	public void setEmployeeId(int x) {
-		this.employeeId = x;
-	}
-
-	public void setHashedPassword(String x) {
-		hashedPassword = x;
-	}
-
-	public void setPassword(String s) {
-		salt = newSalt();
-		hashedPassword = hash(s);
-	}
-
-	public void setSalt(int x) {
-		salt = x;
-	}
-
-	public void setEmployeeFirstName(String s) {
-		employeeFirstName = s;
-	}
-
-	public void setEmployeeLastName(String s) {
-		employeeLastName = s;
-	}
-
-	public void setUsername(String s) {
-		username = s.trim();
-	}
-
+	public int getEmployee_id() {	return this.employee_id; }
+	public String getPassword()     { return password;  }
+	public String  getUsername()       { return username;       }
+	public int     getSalt()           { return salt;           }
+	public String  getFirstname()      { return firstname;      }
+	public String  getLastname()       { return lastname;       }
+	public int     hashCode()          { return username.hashCode(); }
+	
+	
+	public void setEmployee_id (int x ) { this.employee_id = x;}
+	public void setPassword(String s)     {this.password = s;}
+	public void setSalt(int x)               { salt = x;           }
+	public void setFirstname(String s)       { firstname = s;      }
+	public void setLastname(String s)        { lastname = s;       }
+	public void setUsername(String s)        { username = s.trim();       }
+	
 	public String toString() {
-		return "employee(" + getUsername() + ")";
+		return "employee("+getUsername()+")";
 	}
 
 	private String hash(String clearPassword) {
-		if (salt == 0)
-			return null;
+		if (salt == 0) return null;
 
 		MessageDigest md = null;
 		try {
-			md = MessageDigest.getInstance("SHA1");
+		  md = MessageDigest.getInstance("SHA1");
 		} catch (NoSuchAlgorithmException e) {
-			throw new AssertionError(
-					"Can't find the SHA1 algorithm in the java.security package");
+		  throw new AssertionError("Can't find the SHA1 algorithm in the java.security package");
 		}
 
 		String saltString = String.valueOf(salt);
-
+		
 		md.update(saltString.getBytes());
 		md.update(clearPassword.getBytes());
 		byte[] digestBytes = md.digest();
 
 		// Format the digest as a String
 		StringBuffer digestSB = new StringBuffer();
-		for (int i = 0; i < digestBytes.length; i++) {
-			int lowNibble = digestBytes[i] & 0x0f;
-			int highNibble = (digestBytes[i] >> 4) & 0x0f;
-			digestSB.append(Integer.toHexString(highNibble));
-			digestSB.append(Integer.toHexString(lowNibble));
+		for (int i=0; i<digestBytes.length; i++) {
+		  int lowNibble = digestBytes[i] & 0x0f;
+		  int highNibble = (digestBytes[i]>>4) & 0x0f;
+		  digestSB.append(Integer.toHexString(highNibble));
+		  digestSB.append(Integer.toHexString(lowNibble));
 		}
 		String digestStr = digestSB.toString();
 
@@ -132,11 +93,8 @@ public class Employee implements Comparable<Employee> {
 
 	private int newSalt() {
 		Random random = new Random();
-		return random.nextInt(8192) + 1; // salt cannot be zero
+		return random.nextInt(8192)+1;  // salt cannot be zero
 	}
 
-	public Object getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
+

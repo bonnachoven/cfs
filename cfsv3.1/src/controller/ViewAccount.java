bf1162fc8259c49118698e918ff6649e5b6c39c3
Eddie <mitchneeil@gmail.com>
2015-01-28 10:56:45 -0500

    fix view

--- cfsv3.1/src/controller/ViewAccount.java
+++ cfsv3.1/src/controller/ViewAccount.java
@@ -1,10 +1,10 @@
+
 package controller;
 
 import java.text.DecimalFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,12 +17,13 @@ import model.Model;
 import model.PositionDAO;
 import model.TransactionDAO;
 
+import org.apache.taglibs.standard.extra.spath.ParseException;
 import org.genericdao.RollbackException;
 
 import databean.Customer;
+import databean.Position;
 import databean.Fund;
 import databean.Fund_Price_History;
-import databean.Position;
 import databean.TransactionBean;
 import databean.ViewAccountRecord;
 
@@ -53,45 +54,36 @@ public class ViewAccount extends Action {
 		List<String> errors = new ArrayList<String>();
 		HttpSession session = request.getSession(false);
 		session.setAttribute("errors", errors);
-		DecimalFormat cash = new DecimalFormat(	"###,##0.00");
-		
 
 		// get customer
 		Customer customer = (Customer) session.getAttribute("customer");
-		request.setAttribute("cash",cash.format(customer.getCash() / 100.0));
 
 		if (customer == null)
 			return "login.jsp";
 
 		// get last transaction day
 		try {
+			DecimalFormat cash = new DecimalFormat(	"###,##0.00");
 			TransactionBean[] transactionlist = transactionDAO
 					.getTransactions(customer.getCustomer_id());
+			request.setAttribute("cash",cash.format(customer.getCash() / 100.0));
 			if (transactionlist.length == 0) {
 				session.setAttribute("lastTransactionDay",
 						"There is no record of any transaction");
-			} else {
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-				Date lastTransactionDay = sdf.parse(transactionlist[0]
-						.getExecute_date());
-				for (TransactionBean transaction : transactionlist) {
-					if (sdf.parse(transaction.getExecute_date()).after(
-							lastTransactionDay)) {
-						lastTransactionDay = sdf.parse(transaction
-								.getExecute_date());
-					}
-				}
-				session.setAttribute("lastTransactionDay",
-						sdf.format(lastTransactionDay));
-			}
+			} //below please change for me
+//				else {
+//				Date lastTransactionDay = transactionlist[0].getExecute_date();
+//				for (TransactionBean transaction : transactionlist) {
+//					if (transaction.getExecute_date().after(lastTransactionDay)) {
+//						lastTransactionDay = transaction.getExecute_date();
+//					}
+//				}
+//				session.setAttribute("lastTransactionDay",
+//						lastTransactionDay.toString());
+//			}
 
 		} catch (RollbackException e) {
 			e.printStackTrace();
-			return "error.jsp";
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "error.jsp";
 		}
 
 		// get position and fund values
@@ -100,6 +92,7 @@ public class ViewAccount extends Action {
 					.getCustomer_id());
 			ArrayList<ViewAccountRecord> records = new ArrayList<ViewAccountRecord>();
 			Double value = 0.0;
+			DecimalFormat cash = new DecimalFormat(	"###,##0.00");
 			for (Position position : positionList) {
 				int fund_id = position.getFund_id();
 				long shares = position.getShares();
@@ -124,38 +117,44 @@ public class ViewAccount extends Action {
 				ViewAccountRecord record = new ViewAccountRecord();
 				// Format price
 				String sLatestPrice = String.valueOf(latestprice);
-				record.setCurrentPrice(sLatestPrice.substring(0,
-						sLatestPrice.length() - 2)
+				record.setCurrentPrice(sLatestPrice.substring(0,sLatestPrice.length() - 2)
 						+ "."
 						+ sLatestPrice.substring(sLatestPrice.length() - 2));
-
+				
+				
+				
+				
 				// Format shares
 				String sShares = String.valueOf(shares);
 				record.setShares(sShares.substring(0, sShares.length() - 3)
-						+ "." + sShares.substring(sShares.length() - 3));
-
+						+ "." 
+						+ sShares.substring(sShares.length() - 3));
+				
+				
 				// count value
-				System.out
-						.println("Double.value of record.getCurrentPrice() = "
-								+ Double.valueOf(record.getCurrentPrice()));
+				System.out.println("Double.value of record.getCurrentPrice() = " + Double.valueOf(record.getCurrentPrice()));
 				Double dPrice = Double.valueOf(record.getCurrentPrice().trim());
 				Double dShare = Double.valueOf(record.getShares().trim());
 				value += dPrice * dShare;
 
+				
+				
 				// set fund name
 				record.setFundName(fund.getName());
 				// set ticker
 				record.setFundTicker(fund.getSymbol());
 				records.add(record);
-
+				
 			}
 			session.setAttribute("records", records);
 			session.setAttribute("value",cash.format(value));
 			System.out.println("total value = " + value);
+			
+ 			
 
 		} catch (RollbackException e) {
 			e.printStackTrace();
-		}
+		} 
 
 		return "view-account.jsp";
 	}

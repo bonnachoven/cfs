0347a39714338e5d9d1f4a4fc788607067ca8088
Jhalakg <jhalakgoyal89@gmail.com>
2015-01-27 20:25:48 -0500

    fixed files with validation error

--- cfsv3.1/src/formbean/DepositCheckForm.java
+++ cfsv3.1/src/formbean/DepositCheckForm.java
@@ -2,18 +2,17 @@ package formbean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.mybeans.form.FormBean;
 
 public class DepositCheckForm extends FormBean {
 	private String amount;
-
+	private double amt;
 
 	public String getAmount() {
 		return amount;
 	}
-
-
 
 	public void setAmount(String amount) {
 		this.amount = amount.trim();
@@ -24,13 +23,34 @@ public class DepositCheckForm extends FormBean {
 
 		if (amount == null || amount.length() == 0) {
 			errors.add("Amount is required");
-			return errors;
+		} else if (amount.matches(".*[<>\"].*")) {
+			errors.add("Amount cannot contain angle brackets or quotes");
 		}
-		if (errors.size() > 0) {
+
+		if (errors.size() > 0)
 			return errors;
+
+		try {
+			amt = Double.parseDouble(amount);
+			amt = (amt * 100);
+			amt = amt / 100;
+			if (!checkDecimal(amount)) {
+				errors.add("Amount should be in x.xx format(tracked to two decimal places).");
+			} else if (amt < 1) {
+				errors.add("Please enter an amount that is greater than $1");
+			} else if (amt > 1000000000) {
+				errors.add("Please enter an amount that is lesser than $1000000000");
+			}
+		} catch (NumberFormatException nfe) {
+			errors.add("Amount must be numerical");
 		}
 		return errors;
 
 	}
-	
+
+	public boolean checkDecimal(String input) {
+		Pattern p = Pattern.compile("[+-]?[0-9]+.{0,1}[0-9]{0,2}");
+		return p.matcher(input).matches();
+	}
+
 }
--- cfsv3.1/src/formbean/RequestCheckForm.java
+++ cfsv3.1/src/formbean/RequestCheckForm.java
@@ -1,17 +1,19 @@
-package formbean;	
+package formbean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.mybeans.form.FormBean;
 
 public class RequestCheckForm extends FormBean {
 	private String amount;
-
+	private double amt;
 	public String getAmount() {
 		return amount;
 	}
-// hope this would not create a problem
+
+	// hope this would not create a problem
 	public long getAmountAsLong() {
 		// Be sure to first call getValidationErrors() to ensure
 		// that NullPointer exception or NumberFormatException will not be
@@ -28,20 +30,35 @@ public class RequestCheckForm extends FormBean {
 
 		if (amount == null || amount.length() == 0) {
 			errors.add("Amount is required");
-			return errors;
+		
+		} else if (amount.matches(".*[<>\"].*")) {
+			errors.add("Amount cannot contain angle brackets or quotes");
 		}
-
+		
+		if (errors.size() > 0)
+			return errors;
+		
 		try {
-		 long amt = Long.parseLong(amount.trim());
-			if (amt <0 || amt==0) {
-				errors.add("Amount cannot be negative!");
-				
+			amt = Double.parseDouble(amount);
+			amt = (amt * 100);
+			amt = amt / 100;
+			if (!checkDecimal(amount)) {
+				errors.add("Amount should be in x.xx format(tracked to two decimal places).");
 			}
-		} catch (NumberFormatException e) {
-			errors.add("Amount is not an integer");
+			else if (amt < 1) {
+				errors.add("Please enter an amount that is greater than $1");
+			} else if (amt > 1000000000) {
+				errors.add("Please enter an amount that is lesser than $1000000000");
+			}
+		} catch (NumberFormatException nfe) {
+			errors.add("Amount must be numerical");
 		}
 		return errors;
+	}
 
+	public boolean checkDecimal(String input) {
+		Pattern p = Pattern.compile("[+-]?[0-9]+.{0,1}[0-9]{0,2}");
+		return p.matcher(input).matches();
 	}
 
 }
